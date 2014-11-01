329d91b7620fe3e569869d74d00b90a91b314303
c0state <stephen.liu@alumni.stanford.edu>
2014-10-31 22:56:29 -0400

    Add script to install homebrew packages

---
+++ etc/brew_update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# exit on any failed command
+set -e
+
+brew update && brew upgrade
+
+# install cask - https://github.com/caskroom/homebrew-cask
+brew install caskroom/cask/brew-cask
+
+# install cask managed binaries
+brew install homebrew/binary/packer
+
+brew install ack
+brew install ag
+brew install bash
+brew install --default-names findutils
+brew install git
+brew install htop
+brew install mono
+brew install node
+brew install npm
+brew install pyenv-virtualenv pyenv-virtualenvwrapper
+brew install python python3
+brew install tmux
+brew install tree
+brew install wget
+
+echo "Finished installing brew packages successfully."
