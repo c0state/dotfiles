54ecd5327ca19f0c0c23db7dea32735185f45dbf
c0state <stephen.liu@alumni.stanford.edu>
2015-04-14 22:38:09 -0400

    Add pythonpy

--- etc/pip_install.sh
+++ etc/pip_install.sh
@@ -2,3 +2,4 @@ curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python
 easy_install pip
 pip install --upgrade virtualenvwrapper
 pip install --upgrade cdiff
+pip install --upgrade pythonpy
