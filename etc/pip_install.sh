4ff1c6d013bda2fa1ca12f2cb57e2b1ee82327f3
c0state <stephen.liu@alumni.stanford.edu>
2015-12-11 23:15:52 -0500

    Add awscli to list of pip packages

--- etc/pip_install.sh
+++ etc/pip_install.sh
@@ -1,4 +1,5 @@
 # this is a list of pypi packages to install into system python for general use
+pip install --upgrade awscli
 pip install --upgrade cdiff
 pip install --upgrade jupyter
 pip install --upgrade pythonpy
