90e15836baf2013676819fb78947852fb9ef82de
c0state <stephen.liu@alumni.stanford.edu>
2015-12-22 16:42:48 -0500

    Update script that syncs the tool repos (eg: oh-my-zsh, vundle, etc.)

--- etc/sync_repos.py
+++ etc/sync_repos.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 
-# This script syncs the external repos used by these dotfiles (e.g.: oh-my-zsh, vundle, etc.)
+# This script syncs the external repos used by these dotfiles
+# (e.g.: oh-my-zsh, vundle, etc.)
 
 import os
-import getpass
-from optparse import OptionParser
 
-repos = ('~/.oh-my-zsh', '~/.vim/bundle/vundle', '~/.z')
+repos = {
+    '~/.oh-my-zsh': 'https://github.com/robbyrussell/oh-my-zsh.git',
+    '~/.vim/bundle/vundle.vim':
+        'https://github.com/VundleVim/Vundle.vim.git',
+    '~/.z.git': 'https://github.com/rupa/z.git',
+}
+
+
+def clone_tools_repos():
+    for (repo_dir, repo_github_url) in repos.iteritems():
+        repo_dir = os.path.expanduser(repo_dir)
+        if not os.path.exists(repo_dir):
+            os.system('git clone {} {}'.format(repo_github_url, repo_dir))
+
+
+def pull_tools_repos():
+    for (repo_dir, repo_github_url) in repos.iteritems():
+        print("---------- Updating repo [{}] from [{}]".format(
+            repo_dir, repo_github_url))
+        os.system('(cd {} && git pull)'.format(repo_dir))
+        print("---------- Done updating repo [{}]".format(repo_dir))
 
 if __name__ == '__main__':
-    for repo in repos:
-        print("---------- Updating repo [%s]" % (repo))
-        os.system('(cd %s && git pull)' % (repo))
-        print("---------- Done updating repo [%s]" % (repo))
+    clone_tools_repos()
+    pull_tools_repos()
