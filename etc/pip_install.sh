0702dfb0b506dafb598cfb2badd336ae89c1f125
Stephen Liu <stephen@sherpaa.com>
2016-05-22 11:09:37 -0400

    Add pip packages ansible-toolkit, http-prompt, neovim and sshuttle

--- etc/pip_install.sh
+++ etc/pip_install.sh
@@ -1,4 +1,5 @@
 # this is a list of pypi packages to install into system python for general use
+pip install --upgrade ansible-toolkit
 pip install --upgrade autoenv
 pip install --upgrade autopep8
 pip install --upgrade awscli
@@ -7,9 +8,11 @@ pip install --upgrade cookiecutter
 pip install --upgrade csvkit
 pip install --upgrade gunicorn
 pip install --upgrade howdoi
+pip install --upgrade http-prompt
 pip install --upgrade httpie
-pip install --upgrade markdown
 pip install --upgrade jupyter
+pip install --upgrade markdown
+pip install --upgrade neovim
 pip install --upgrade pip
 pip install --upgrade pip-tools
 pip install --upgrade ptpython
@@ -17,5 +20,6 @@ pip install --upgrade pytest
 pip install --upgrade pythonpy
 pip install --upgrade pyyaml
 pip install --upgrade saws
+pip install --upgrade sshuttle
 pip install --upgrade virtualenv
 pip install --upgrade virtualenvwrapper
