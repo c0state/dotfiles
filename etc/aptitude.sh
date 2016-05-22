7d52f5409af3f8a518052986f35fb2088e42b47d
Stephen Liu <stephen@sherpaa.com>
2016-05-22 11:18:45 -0400

    Add/change packages for Debian install script
    Use vim-gnome instead of vim package
    Install cmake, golang, mono
    Install pyenv dependencies

--- etc/aptitude.sh
+++ etc/aptitude.sh
@@ -1,4 +1,10 @@
 # packages to install on ubuntu
 
 apt-get install aptitude
-aptitude install curl git gparted vim wget zsh -y
+aptitude install curl git gparted vim-gnome wget zsh -y
+aptitude install cmake -y
+aptitude install golang -y
+# mono
+aptitude install mono-mcs mono-runtime mono-xbuild -y
+# pyenv
+aptitude install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev -y
