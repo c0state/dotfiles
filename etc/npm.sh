c3990655fd959899b258381689b3f81fca2a94d7
Stephen Liu <stephen.liu@alumni.stanford.edu>
2016-07-27 00:08:42 -0400

    Exit immediately if npm or ruby scripts fail

--- etc/npm.sh
+++ etc/npm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 npm install -g bower
 npm install -g diff-so-fancy
 npm install -g instant-markdown-d
--- etc/ruby.sh
+++ etc/ruby.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+set -ex
+
 gem install terraforming
 gem install tmuxinator
