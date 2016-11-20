9d8236614d2f970d79bf56a5be21aa054f3b8c61
c0state <stephen.liu@alumni.stanford.edu>
2016-11-20 15:25:37 -0500

    Add origami studio and sketch toolbox cask packages

--- etc/brew_update.sh
+++ etc/brew_update.sh
@@ -93,9 +93,11 @@ brew cask install java
 brew cask install lastpass
 brew cask install libreoffice
 brew cask install logitech-harmony
+brew cask install origami-studio
 brew cask install pycharm
 brew cask install robomongo
 brew cask install rubymine
+brew cask install sketch-toolbox
 brew cask install slack
 brew cask install sourcetree
 brew cask install sqlitebrowser
