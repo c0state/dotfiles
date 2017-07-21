7f1ef1aba1f3c1db92fde4dcc90ca9e1ab6c0f11
Stephen Liu <stephen.liu@alumni.stanford.edu>
2017-07-21 13:23:20 -0400

    Add “dynamodb-admin” npm global package

--- etc/npm.sh
+++ etc/npm.sh
@@ -5,6 +5,7 @@ set -ex
 npm install -g beame-insta-ssl
 npm install -g bower
 npm install -g diff-so-fancy
+npm install -g dynamodb-admin
 npm install -g instant-markdown-d
 npm install -g tldr
 npm install -g typescript
