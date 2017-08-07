3c72d706648f026a8462fc970092ad43b53cfec4
Stephen Liu <stephen.liu@alumni.stanford.edu>
2017-08-07 18:04:54 -0400

    Add imageoptim-cli npm package

--- etc/npm.sh
+++ etc/npm.sh
@@ -6,6 +6,7 @@ npm install -g beame-insta-ssl
 npm install -g bower
 npm install -g diff-so-fancy
 npm install -g dynamodb-admin
+npm install -g imageoptim-cli
 npm install -g instant-markdown-d
 npm install -g tldr
 npm install -g typescript
