2152a8eb746abccdb5ac578d6d778170ca1c0f36
Stephen Liu <stephen@storymotions.com>
2017-12-09 19:06:07 -0500

    Add various web framework tools—vue, yarn, create react app—to npm packages

--- etc/npm.sh
+++ etc/npm.sh
@@ -5,10 +5,13 @@ set -ex
 npm install -g aws-sam-local
 npm install -g beame-insta-ssl
 npm install -g bower
+npm install -g create-react-app
 npm install -g diff-so-fancy
 npm install -g dynamodb-admin
 npm install -g imageoptim-cli
 npm install -g instant-markdown-d
 npm install -g tldr
 npm install -g typescript
+npm install -g vue-cli
 npm install -g webpack
+npm install -g yarn
