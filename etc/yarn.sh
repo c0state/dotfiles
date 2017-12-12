e69801a3c889e9bd34435098c872df6f9656262d
Stephen Liu <stephen@storymotions.com>
2017-12-12 11:17:27 -0500

    Convert npm script to yarn

---
+++ etc/yarn.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+yarn global add aws-sam-local
+yarn global add beame-insta-ssl
+yarn global add create-react-app
+yarn global add diff-so-fancy
+yarn global add dynamodb-admin
+yarn global add imageoptim-cli
+yarn global add instant-markdown-d
+yarn global add poi
+yarn global add tldr
+yarn global add typescript
+yarn global add vue-cli create-vue-app
+yarn global add webpack
+yarn global add yarn
