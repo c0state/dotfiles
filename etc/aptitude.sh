23251c588ad0e067a038141162849b6e34eb3f6d
Stephen Liu <stephen@storymotions.com>
2019-03-10 18:45:10 -0400

    Switch to curl for mono GPG key installation, add colordiff, switch from vim-gnome to vim-nox, update to openjdk-11, move nodejs-legacy

--- etc/aptitude.sh
+++ etc/aptitude.sh
@@ -6,7 +6,7 @@ set -ex
 
 #----- set up keys and repos
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+curl https://download.mono-project.com/repo/xamarin.gpg | sudo apt-key add -
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
 
 #----- update
@@ -17,7 +17,7 @@ apt-get update
 
 apt-get install aptitude
 aptitude install -y \
-    curl git gparted vim-gnome wget \
+    colordiff curl git gparted vim-nox wget \
     direnv \
     dos2unix \
     golang \
@@ -32,12 +32,12 @@ aptitude install -y \
 
 # install java packages
 aptitude install -y \
-    openjdk-8-jdk openjdk-9-jdk
+    openjdk-8-jdk openjdk-11-jdk
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
 # install node packages
 aptitude install -y \
-    nodejs nodejs-legacy npm
+    nodejs npm
 
 # install python packages
 aptitude install -y \
