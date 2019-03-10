a39e95f9206737e83e5f4c3554db2c81b3e81152
Stephen Liu <stephen@storymotions.com>
2019-03-10 13:49:47 -0400

    Add jigsaw outline client to mac app store install script

--- etc/app_store.sh
+++ etc/app_store.sh
@@ -1,6 +1,6 @@
 mas signin --dialog
 
-mas install 497799835
+mas install 497799835 # xcode
 
 # set xcode tools to full version; see: https://stackoverflow.com/a/17980786
 echo "Setting full version of XCode tools"
@@ -8,3 +8,5 @@ sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 
 echo "Accepting XCode license"
 sudo xcodebuild -license accept
+
+mas install 1356178125 # jigsaw outline client
