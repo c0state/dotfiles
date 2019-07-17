d6cc8e5caa2832bd0ff1d2cc4442dcf38126ea92
Stephen Liu <stephen.liu@alumni.stanford.edu>
2019-07-16 21:07:59 -0400

    Use "stable" for mono project dependency on linux

--- etc/aptitude.sh
+++ etc/aptitude.sh
@@ -27,7 +27,7 @@ aptitude install -y \
 #----- install mono
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb https://download.mono-project.com/repo/ubuntu stable main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 
 aptitude update && aptitude install -y \
     mono-complete
