68948113ba9d96d99183939851c22e1cf83525fa
Stephen Liu <sliu@quantum-si.com>
2019-10-15 20:03:30 -0400

    pip and setuptools should not be installed with —user flag
    also add pyupgrade python package

--- etc/pip_install.sh
+++ etc/pip_install.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# install pip and setuptools into normal location
+pip install --upgrade pip setuptools
+
 # this is a list of pypi packages to install into system python for general use
 pip install --upgrade --user ansible ansible-toolkit markupsafe
 pip install --upgrade --user autoenv
@@ -20,13 +23,13 @@ pip install --upgrade --user httpie
 pip install --upgrade --user jupyter
 pip install --upgrade --user markdown
 pip install --upgrade --user neovim pynvim
-pip install --upgrade --user pip
 pip install --upgrade --user pipenv
 pip install --upgrade --user pip-tools
 pip install --upgrade --user ptpython
 pip install --upgrade --user pylint
 pip install --upgrade --user pytest
 pip install --upgrade --user pythonpy
+pip install --upgrade --user pyupgrade
 pip install --upgrade --user pyyaml
 pip install --upgrade --user ranger-fm
 pip install --upgrade --user virtualenv
