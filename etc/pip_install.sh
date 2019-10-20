8de8886cdf6e6bcee3053baaf7b4ece8ed7d3bcf
Stephen Liu <sliu@quantum-si.com>
2019-10-19 23:09:19 -0400

    Remove pip packages that should be installed in projects, not system wide

--- etc/pip_install.sh
+++ etc/pip_install.sh
@@ -8,7 +8,6 @@ pip install --upgrade pip setuptools
 # this is a list of pypi packages to install into system python for general use
 pip install --upgrade --user ansible ansible-toolkit markupsafe
 pip install --upgrade --user autoenv
-pip install --upgrade --user autopep8
 pip install --upgrade --user aws-shell
 pip install --upgrade --user cdiff
 pip install --upgrade --user cookiecutter
@@ -16,7 +15,6 @@ pip install --upgrade --user csvkit
 pip install --upgrade --user docker-compose
 pip install --upgrade --user dopy
 pip install --upgrade --user jedi
-pip install --upgrade --user gunicorn
 pip install --upgrade --user howdoi
 pip install --upgrade --user http-prompt
 pip install --upgrade --user httpie
@@ -26,8 +24,6 @@ pip install --upgrade --user neovim pynvim
 pip install --upgrade --user pipenv
 pip install --upgrade --user pip-tools
 pip install --upgrade --user ptpython
-pip install --upgrade --user pylint
-pip install --upgrade --user pytest
 pip install --upgrade --user pythonpy
 pip install --upgrade --user pyupgrade
 pip install --upgrade --user pyyaml
