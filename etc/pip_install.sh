44398477ec40862085bf98b643db9be48accacd4
Stephen Liu <sliu@quantum-si.com>
2019-10-22 18:36:38 -0400

    Remove awsshell and http-prompt
    These projects look dead

--- etc/pip_install.sh
+++ etc/pip_install.sh
@@ -8,7 +8,6 @@ pip install --upgrade pip setuptools
 # this is a list of pypi packages to install into system python for general use
 pip install --upgrade --user ansible ansible-toolkit markupsafe
 pip install --upgrade --user autoenv
-pip install --upgrade --user aws-shell
 pip install --upgrade --user cdiff
 pip install --upgrade --user cookiecutter
 pip install --upgrade --user csvkit
@@ -16,7 +15,6 @@ pip install --upgrade --user docker-compose
 pip install --upgrade --user dopy
 pip install --upgrade --user jedi
 pip install --upgrade --user howdoi
-pip install --upgrade --user http-prompt
 pip install --upgrade --user httpie
 pip install --upgrade --user jupyter
 pip install --upgrade --user markdown
