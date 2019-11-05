92e63f8d227d569fc1ca388f5e77a81b85f4b0e8
Stephen Liu <stephen@alumni.stanford.edu>
2019-11-04 22:09:03 -0500

    Add pipx and update python tools install script

---
+++ etc/python_tools_install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+PLATFORM=`uname`
+
+set -ex
+
+python3 -m pip install --upgrade --user pip setuptools
+python3 -m pip install --upgrade --user pipx
+
+# macOS Docker app includes docker-compose
+if [[ $PLATFORM != 'Darwin' ]]; then
+    pipx install docker-compose
+    pipx install neovim
+fi
+
+pipx install ansible
+pipx install ansible-toolkit
+pipx install autoenv
+pipx install black
+pipx install cdiff
+pipx install cookiecutter
+pipx install csvkit
+pipx install fourmat
+pipx install howdoi
+pipx install httpie
+pipx install --include-deps jupyter
+pipx install markdown
+# pipx install pynvim
+pipx install pipenv
+pipx install pip-tools
+pipx install ptpython
+pipx install pythonpy
+pipx install pyupgrade
+pipx install ranger-fm
+
+pipx upgrade-all
