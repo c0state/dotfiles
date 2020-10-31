fc77107a5bf1f930a8ad86d0b4f07dc1fea72135
Stephen <stephen.liu@alumni.stanford.edu>
2020-10-31 17:14:18 -0400

    add windows package install scripts

---
+++ windows_configs/scoop_update.ps1
@@ -0,0 +1,24 @@
+scoop bucket add extras
+scoop bucket add nerd-fonts
+
+scoop install sudo
+
+scoop install 7zip
+scoop install audacity
+scoop install coretemp
+scoop install cpu-z
+scoop install dotnet-sdk
+sudo scoop install firacode --global
+sudo scoop install hack-nf --global
+scoop install etcher
+scoop install firefox
+scoop install git
+scoop install googlechrome
+scoop install neovim
+scoop install nsudo
+scoop install vlc
+scoop install vscode
+
+scoop install bucket/jetbrains-toolbox 
+
+scoop reset *
---
+++ windows_configs/winget_update.ps1
@@ -0,0 +1,18 @@
+winget install 7zip
+winget install audacity
+winget install coretemp
+winget install cpu-z
+winget install ".net core sdk"
+#winget install firacode
+winget install hackfonts
+winget install etcher
+winget install mozilla.firefox
+winget install git
+winget install "github cli"
+winget install "github desktop"
+winget install "google chrome"
+winget install sourcetree
+winget install toolbox 
+winget install vim
+winget install "vlc media player"
+winget install "visual studio code"
