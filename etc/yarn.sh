9f88bb6a01b570522aa6682b33858b63c39997ea
Stephen Liu <sliu@quantum-si.com>
2020-11-18 17:32:52 -0500

    restructure global yarn cli tool install command for better efficiency

--- etc/yarn.sh
+++ etc/yarn.sh
@@ -2,27 +2,28 @@
 
 set -ex
 
-yarn global add aws-cdk
-yarn global add create-react-app
-yarn global add detox-cli
-yarn global add diff-so-fancy
-yarn global add dynamodb-admin
-yarn global add expo-cli
-yarn global add firebase-tools
-yarn global add gatsby-cli
-yarn global add imageoptim-cli
-yarn global add instant-markdown-d
-yarn global add lerna
-yarn global add np
-yarn global add poi
-yarn global add prettier
-yarn global add react-native
-yarn global add renovate
-yarn global add tldr
-yarn global add tslab
-yarn global add typescript
-yarn global add @vue/cli create-vue-app
-yarn global add webpack
-yarn global add yalc
-yarn global add yarn-deduplicate
+yarn global add \
+    aws-cdk \
+    create-react-app \
+    detox-cli \
+    diff-so-fancy \
+    dynamodb-admin \
+    expo-cli \
+    firebase-tools \
+    gatsby-cli \
+    imageoptim-cli \
+    instant-markdown-d \
+    lerna \
+    np \
+    poi \
+    prettier \
+    react-native \
+    renovate \
+    tldr \
+    tslab \
+    typescript \
+    @vue/cli create-vue-app \
+    webpack \
+    yalc \
+    yarn-deduplicate
 
