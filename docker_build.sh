452fcc11dccd03fa4d6dad047fbcbb260dc2af8f
Stephen <stephen.liu@alumni.stanford.edu>
2021-01-31 14:55:56 -0500

    actually remove all intermediate docker build targets

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# build these targets if you need to debug
-# docker build --tag dotfiles-base --target dotfiles-base .
-# docker build --tag dotfiles-tools --target dotfiles-tools .
-
 docker build --tag dotfiles .
