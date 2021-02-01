0a064fa692b2034a3f22e9cba31f498ebd0c2d66
Stephen <stephen.liu@alumni.stanford.edu>
2021-01-31 19:17:50 -0500

    need libssl-dev dependency for rbenv install command

--- etc/aptitude.sh
+++ etc/aptitude.sh
@@ -31,7 +31,7 @@ sudo apt -y update && sudo apt -y install \
 
 # install developer packages
 sudo apt -y install \
-    build-essential cmake zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev libxml2-dev \
+    build-essential cmake zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libssl-dev llvm libncurses5-dev libncursesw5-dev libxml2-dev \
     neovim \
     vagrant
 
