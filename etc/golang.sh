66a7a7f9da8744be6682769b293ad0638c8257ff
Stephen Liu <stephen@alumni.stanford.edu>
2021-02-27 13:45:44 -0500

    fix go setup script for macos

--- etc/golang.sh
+++ etc/golang.sh
@@ -2,11 +2,25 @@
 
 set -ex
 
-GO_VERSION=1.15.7
+#---------- variables
+
+GO_VERSION=1.16
+PLATFORM=$(uname)
+
+if [[ "$PLATFORM" == "Darwin" ]]; then
+    PLATFORM_STRING="darwin"
+elif [[ "$PLATFORM" == "Linux" ]]; then
+    PLATFORM_STRING="linux"
+else
+    echo Platform "$PLATFORM" not supported
+    exit 1
+fi
+
+#---------- install section
 
 if ! command -v go >/dev/null; then
     mkdir -p ~/.local
-    wget -qO- https://golang.org/dl/go"$GO_VERSION".linux-amd64.tar.gz | tar zxvf - -C $HOME/.local
+    wget -qO- https://golang.org/dl/go"$GO_VERSION"."$PLATFORM_STRING"-amd64.tar.gz | tar zxvf - -C "$HOME"/.local
 fi
 
 go get -u github.com/motemen/gore/cmd/gore
