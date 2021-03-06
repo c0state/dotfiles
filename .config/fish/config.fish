99eb28333c33b0afbfa01bacf38bb6b1344ff157
stephen.liu@alumni.stanford.edu <stephen.liu@alumni.stanford.edu>
2021-03-06 15:17:38 -0500

    fix fish pyenv config so it doesn't mangle path on every startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ starship init fish | source
 #---------- pyenv
 
 set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+contains $PYENV_ROOT/bin $fish_user_paths; or set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
 # pyenv init
 if command -v pyenv 1>/dev/null 2>&1
