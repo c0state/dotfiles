95829ff03a6c69a07690a467de7a36f950bcf5b7
Stephen Liu <sliu@quantum-si.com>
2021-03-07 12:01:42 -0500

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,9 @@ starship init fish | source
 bind --erase \cf
 bind \co __fzf_search_current_dir
 
-#---------- pyenv
-
-set -Ux PYENV_ROOT $HOME/.pyenv
-contains $PYENV_ROOT/bin $fish_user_paths; or set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+set PYENV_ROOT ~/.pyenv;
+set PATH $PATH $PYENV_ROOT/bin;
+set PATH $PATH ~/.local/bin;
 
 # pyenv init
 if command -v pyenv 1>/dev/null 2>&1
