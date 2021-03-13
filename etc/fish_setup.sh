15401000f6c46ef4a0fb01571d3200560bb28137
stephen.liu@alumni.stanford.edu <stephen.liu@alumni.stanford.edu>
2021-03-12 22:20:25 -0500

    consolidate current fish scripts

--- etc/fish_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-if ! type -q fisher
-    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-end
-
-fisher install evanlucas/fish-kubectl-completions
-fisher install PatrickF1/fzf.fish
-
--- etc/setup_env.fish
+++ etc/setup_env.fish
@@ -1,5 +1,15 @@
 #!/usr/bin/env fish
 
+#---------- fisher
+
+# TODO: do we still need fisher?
+
+#if ! type -q fisher
+#    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+#end
+#
+#fisher install evanlucas/fish-kubectl-completions
+
 #---------- oh-my-fish
 
 if not type -q omf
