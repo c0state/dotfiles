1de2f2952e20c945cf58a36b9a9c9fc095668165
Stephen <stephen.liu@alumni.stanford.edu>
2021-03-21 23:51:44 -0400

    add faststone viewer to winget

--- windows_configs/winget_update.ps1
+++ windows_configs/winget_update.ps1
@@ -3,24 +3,25 @@ winget install audacity
 winget install coretemp
 winget install cpu-z
 winget install docker.dockerdesktop
-# not yet available
-#winget install firacode
-winget install hackfonts
 winget install etcher
-winget install mozilla.firefox
+winget install faststone.viewer
+# not yet available
+# winget install firacode
 winget install git
 winget install github.cli
 winget install github.githubdesktop
 winget install google.backupandsync
 winget install google.chrome
 winget install google.drivefilestream
+winget install hackfonts
 winget install kubernetes.minikube
 winget install microsoft.dotnet
+winget install microsoft.visualstudiocode-user-x64
 winget install mirantis.lens
+winget install mozilla.firefox
 # winget install neovim
 winget install sourcetree
 winget install toolbox 
-winget install vim
 winget install videolan.vlc
-winget install microsoft.visualstudiocode-user-x64
+winget install vim
 winget install windirstat.windirstat
