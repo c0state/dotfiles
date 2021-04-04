d1f04674b354a0d5d82f352716d51a0717c6d2bf
Stephen <stephen.liu@alumni.stanford.edu>
2021-04-03 22:34:54 -0400

    remove old k8s setup script

--- etc/setup_kubernetes.sh
+++ etc/setup_kubernetes.sh
@@ -4,7 +4,7 @@ set -eux
 
 # add kubernetes repos
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo bash -c "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg > /usr/share/keyrings/kubernetes-archive-keyring.gpg"
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg -o /etc/apt/trusted.gpg.d/google-cloud.gpg
 
 # install dependencies
 sudo apt-get install apt-transport-https ca-certificates gnupg
