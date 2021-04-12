a983fc0edde48a3596674f9b5641c6e1fa16f73f
Stephen <stephen.liu@alumni.stanford.edu>
2021-04-11 21:33:29 -0400

    fix kubernetes gpg file location

--- etc/setup_kubernetes.sh
+++ etc/setup_kubernetes.sh
@@ -4,7 +4,7 @@ set -eux
 
 # add kubernetes repos
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg -o /etc/apt/trusted.gpg.d/google-cloud.gpg
+sudo curl -LS https://packages.cloud.google.com/apt/doc/apt-key.gpg -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
 
 # install dependencies
 sudo apt-get install apt-transport-https ca-certificates gnupg
