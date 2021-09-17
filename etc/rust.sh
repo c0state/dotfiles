deffb83bc8df3b3818fbed82b5f6392e481b0261
Stephen Liu <sliu@quantum-si.com>
2021-09-17 13:32:28 -0400

    add commands to update rust and rustup in rust setup script

--- etc/rust.sh
+++ etc/rust.sh
@@ -2,6 +2,9 @@
 
 if ! $(command -v rustc >/dev/null 2>&1); then
     curl --proto '=https' --tls-max default -sSf https://sh.rustup.rs | bash -s -- -y
+else
+    rustup update
+    rustup self update
 fi
 
 source "$HOME/.cargo/env"
