bec0e9c422d5bf60c409e47c990e60a0390de821
Stephen <stephen.liu@alumni.stanford.edu>
2021-11-13 23:05:40 -0500

    use bash script for winget package installation

--- windows_configs/winget_update.sh
+++ windows_configs/winget_update.sh
@@ -1,37 +1,41 @@
-winget install --id 7zip
-winget install --id adobe.adobeacrobatreaderdc
-winget install atlassian.sourcetree
-winget install --id audacity
-winget install --id coretemp
-winget install --id cpu-z
-winget install --id discord
+#!/usr/bin/env bash
+
+set -eu
+
+winget install --id 7zip.7zip
+winget install --id adobe.acrobat.reader.64-bit
+winget install --id alcpu.coretemp
+winget install --id atlassian.sourcetree
+winget install --id audacity.audacity
+winget install --id balena.etcher
+winget install --id cpuid.cpu-z
+winget install --id discord.discord
 winget install --id docker.dockerdesktop
-winget install --id etcher
 winget install --id faststone.viewer
 winget install --id flux.flux
-# not yet available
+# TODO: not yet available
 # winget install --id firacode
-winget install --id git
+winget install --id git.git
 winget install --id github.cli
 winget install --id github.githubdesktop
 winget install --id google.chrome
 winget install --id google.drive
-winget install --id hackfonts
+winget install --id jetbrains.toolbox 
 winget install --id kubernetes.minikube
 winget install --id microsoft.dotnet
-winget install --id microsoft.visualstudiocode-user-x64
+winget install --id microsoft.visualstudiocode
 winget install --id mirantis.lens
 winget install --id mozilla.firefox
+# TODO: not yet available
 # winget install --id neovim
 winget install --id obsproject.obsstudio
 winget install --id raspberrypifoundation.raspberrypiimager
+winget install --id randyrants.sharpkeys
 winget install --id rufus.rufus
 winget install --id slacktechnologies.slack
-winget install --id sourcetree
+winget install --id sourcefoundry.hackfonts
 winget install --id teamviewer.teamviewer
-winget install --id toolbox 
 winget install --id videolan.vlc
-winget install --id vim
 winget install --id whatsapp.whatsapp
 winget install --id windirstat.windirstat
 winget install --id wireguard.wireguard
