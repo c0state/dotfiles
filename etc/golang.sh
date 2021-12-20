da0637ef2bcff3ecee1d6abd2bc0b629df169a5f
Stephen <stephen.liu@alumni.stanford.edu>
2021-12-20 08:57:14 -0500

    update golang install script
    fix arch variable and gore install

--- etc/golang.sh
+++ etc/golang.sh
@@ -4,9 +4,14 @@ set -ex
 
 #---------- variables
 
-GO_VERSION=1.17
+GO_VERSION=1.17.5
 PLATFORM=$(uname)
-CPUTYPE=$(uname -m)
+MACH_TYPE=$(uname -m)
+ARCH_TYPE=$MACH_TYPE
+
+if [[ "$MACH_TYPE" == "x86_64" ]]; then
+    ARCH_TYPE="amd64"
+fi
 
 if [[ "$PLATFORM" == "Darwin" ]]; then
     PLATFORM_STRING="darwin"
@@ -21,11 +26,12 @@ fi
 
 if ! command -v go >/dev/null; then
     mkdir -p ~/.local
-    wget -qO- https://golang.org/dl/go"$GO_VERSION"."$PLATFORM_STRING"-"$CPUTYPE".tar.gz | tar zxvf - -C "$HOME"/.local
+    wget -qO- https://golang.org/dl/go"$GO_VERSION"."$PLATFORM_STRING"-"$ARCH_TYPE".tar.gz | tar zxvf - -C "$HOME"/.local
 fi
 
 # go repl
-go get -u github.com/motemen/gore/cmd/gore
+go install github.com/x-motemen/gore/cmd/gore@latest
+go install github.com/mdempsky/gocode@latest   # for code completion
 
 # kail
 bash <( curl -sfL https://raw.githubusercontent.com/boz/kail/master/godownloader.sh) -b "$GOPATH/bin"
