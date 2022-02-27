e27d9cc849f08a706302c6455639159e25654b66
Stephen Liu <sliu@quantum-si.com>
2022-02-26 22:29:10 -0500

    add krew to kubernetes script

--- etc/setup_kubernetes.sh
+++ etc/setup_kubernetes.sh
@@ -4,17 +4,20 @@ set -eu
 
 PLATFORM=$(uname)
 
-if [[ $PLATFORM != 'Linux' ]]; then
-    echo "This script is only for Debian based Linux systems"
-    exit 0
+if [[ $PLATFORM == 'Linux' ]]; then
+    # add kubernetes repos
+    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    sudo curl -LS https://packages.cloud.google.com/apt/doc/apt-key.gpg -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+    
+    # install dependencies
+    sudo apt-get install apt-transport-https ca-certificates gnupg
+    
+    # install kubectl
+    sudo apt-get update && sudo apt-get install kubectl
 fi
 
-# add kubernetes repos
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo curl -LS https://packages.cloud.google.com/apt/doc/apt-key.gpg -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+# install kubectl krew packages
 
-# install dependencies
-sudo apt-get install apt-transport-https ca-certificates gnupg
+kubectl krew index add kvaps https://github.com/kvaps/krew-index
+kubectl krew install kvaps/node-shell
 
-# install kubectl
-sudo apt-get update && sudo apt-get install kubectl
