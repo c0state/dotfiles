1e21b9138da438b55ad27b1f38f91415fbfb00f1
Stephen Liu <sliu@quantum-si.com>
2022-02-26 22:40:58 -0500

    update pyenv init calls

--- .shell_interactive.sh
+++ .shell_interactive.sh
@@ -87,6 +87,7 @@ fi
 export PYENV_ROOT=$HOME/.pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
 fi
