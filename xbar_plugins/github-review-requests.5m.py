f2fcc4961bbc836e8a44f2e935124ccccc7dd434
Stephen Liu <sliu@quantum-si.com>
2023-01-30 23:30:46 -0500

    linting

--- xbar_plugins/github-review-requests.5m.py
+++ xbar_plugins/github-review-requests.5m.py
@@ -35,16 +35,16 @@ if not ACCESS_TOKEN:
 GITHUB_LOGIN = config_section["LOGIN_NAME"]
 
 # (optional) PRs with this label (e.g 'in progress') will be grayed out on the list
-WIP_LABEL = 'wip'
+WIP_LABEL = "wip"
 
 # (optional) Filter the PRs by an organization, labels, etc. E.g 'org:YourOrg -label:dropped'
-FILTERS = ''
+FILTERS = ""
 
 # --------------------------------------------------------------------------------
 
-DARK_MODE = os.environ.get('BitBarDarkMode')
+DARK_MODE = os.environ.get("BitBarDarkMode")
 
-query = '''{
+query = """{
   search(query: "%(search_query)s", type: ISSUE, first: 100) {
     issueCount
     edges {
@@ -69,62 +69,68 @@ query = '''{
       }
     }
   }
-}'''
+}"""
 
 colors = {
-    'inactive': '#b4b4b4',
-    'title': '#ffffff' if DARK_MODE else '#000000',
-    'subtitle': '#586069'}
+    "inactive": "#b4b4b4",
+    "title": "#ffffff" if DARK_MODE else "#000000",
+    "subtitle": "#586069",
+}
 
 
 def execute_query(query):
     headers = {
-        'Authorization': 'bearer ' + ACCESS_TOKEN,
-        'Content-Type': 'application/json'}
-    data = json.dumps({'query': query}).encode('utf-8')
-    req = Request(
-        'https://api.github.com/graphql', data=data, headers=headers)
+        "Authorization": "bearer " + ACCESS_TOKEN,
+        "Content-Type": "application/json",
+    }
+    data = json.dumps({"query": query}).encode("utf-8")
+    req = Request("https://api.github.com/graphql", data=data, headers=headers)
     body = urlopen(req).read()
     return json.loads(body)
 
 
-def search_pull_requests(login, filters=''):
-    search_query = 'type:pr state:open review-requested:%(login)s %(filters)s' % {
-        'login': login, 'filters': filters}
-    response = execute_query(query % {'search_query': search_query})
-    return response['data']['search']
+def search_pull_requests(login, filters=""):
+    search_query = "type:pr state:open review-requested:%(login)s %(filters)s" % {
+        "login": login,
+        "filters": filters,
+    }
+    response = execute_query(query % {"search_query": search_query})
+    return response["data"]["search"]
 
 
 def parse_date(text):
-    date_obj = datetime.datetime.strptime(text, '%Y-%m-%dT%H:%M:%SZ')
-    return date_obj.strftime('%B %d, %Y')
+    date_obj = datetime.datetime.strptime(text, "%Y-%m-%dT%H:%M:%SZ")
+    return date_obj.strftime("%B %d, %Y")
 
 
 def print_line(text, **kwargs):
-    params = ' '.join(['%s=%s' % (key, value) for key, value in kwargs.items()])
-    print('%s | %s' % (text, params) if kwargs.items() else text)
+    params = " ".join(["%s=%s" % (key, value) for key, value in kwargs.items()])
+    print("%s | %s" % (text, params) if kwargs.items() else text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not all([ACCESS_TOKEN, GITHUB_LOGIN]):
-        print_line('⚠ Github review requests', color='red')
-        print_line('---')
-        print_line('ACCESS_TOKEN and GITHUB_LOGIN cannot be empty')
+        print_line("⚠ Github review requests", color="red")
+        print_line("---")
+        print_line("ACCESS_TOKEN and GITHUB_LOGIN cannot be empty")
         sys.exit(0)
 
     response = search_pull_requests(GITHUB_LOGIN, FILTERS)
 
     print_line(f"{str(response['issueCount']).rjust(2)} PRs")
-    print_line('---')
+    print_line("---")
 
-    for pr in [r['node'] for r in response['edges']]:
-        labels = [label['name'] for label in pr['labels']['nodes']]
-        title = '%s - %s' % (pr['repository']['nameWithOwner'], pr['title'])
-        title_color = colors.get('inactive' if WIP_LABEL in labels else 'title')
-        subtitle = '#%s opened on %s by @%s' % (
-            pr['number'], parse_date(pr['createdAt']), pr['author']['login'])
-        subtitle_color = colors.get('inactive' if WIP_LABEL in labels else 'subtitle')
+    for pr in [r["node"] for r in response["edges"]]:
+        labels = [label["name"] for label in pr["labels"]["nodes"]]
+        title = "%s - %s" % (pr["repository"]["nameWithOwner"], pr["title"])
+        title_color = colors.get("inactive" if WIP_LABEL in labels else "title")
+        subtitle = "#%s opened on %s by @%s" % (
+            pr["number"],
+            parse_date(pr["createdAt"]),
+            pr["author"]["login"],
+        )
+        subtitle_color = colors.get("inactive" if WIP_LABEL in labels else "subtitle")
 
-        print_line(title, size=16, color=title_color, href=pr['url'])
+        print_line(title, size=16, color=title_color, href=pr["url"])
         print_line(subtitle, size=12, color=subtitle_color)
-        print_line('---')
+        print_line("---")
