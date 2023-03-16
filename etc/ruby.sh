d7725b3adda4386e41331892b98871d1c33b3259
Stephen Liu <sliu@quantum-si.com>
2023-03-15 23:28:44 -0400

    install latest ruby dynamically

--- etc/ruby.sh
+++ etc/ruby.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-set -ex
-
-LATEST_RUBY_VERSION=3.0.0
+set -eu
 
 if [[ ! -e $HOME/.rbenv ]]; then
-    git clone https://github.com/rbenv/rbenv $HOME/.rbenv
-    (cd $HOME/.rbenv && src/configure && make -C src)
+    git clone https://github.com/rbenv/rbenv "$HOME"/.rbenv
+    (cd "$HOME"/.rbenv && src/configure && make -C src)
 fi
 
 if [[ ! -e $HOME/.rbenv/plugins/rbenv-gemset ]]; then
-    git clone https://github.com/jf/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
+    git clone https://github.com/jf/rbenv-gemset.git "$HOME"/.rbenv/plugins/rbenv-gemset
 fi
 
 if [[ ! -e $HOME/.rbenv/plugins/ruby-build ]]; then
-    git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+    git clone https://github.com/rbenv/ruby-build.git "$HOME"/.rbenv/plugins/ruby-build
 fi
 
 eval "$(rbenv init -)"
 
-if ! (rbenv versions | grep $LATEST_RUBY_VERSION); then
+LATEST_RUBY_VERSION=$(rbenv install --list | grep -E "^\d" | sort | tail -n 1)
+
+if ! (rbenv versions | grep "$LATEST_RUBY_VERSION"); then
     rbenv install "$LATEST_RUBY_VERSION"
 fi
+
+rbenv global "$LATEST_RUBY_VERSION"
 
 gem install --user-install cocoapods
 gem install --user-install fastlane
