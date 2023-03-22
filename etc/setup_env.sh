9e6f2a03ac0aa68c63cc92eefa4feda7c55bcac1
Stephen Liu <sliu@quantum-si.com>
2023-03-22 10:26:04 -0400

    don't install dependencies for lunarvim

--- etc/setup_env.sh
+++ etc/setup_env.sh
@@ -61,7 +61,7 @@ vim +PlugUpdate +PlugUpgrade +UpdateRemotePlugins +qall
 # ---------- set up lunarvim
 
 if ! which lvim > /dev/null; then
-    LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+    LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh) -- --no-install-dependencies
 else
     lvim +LvimUpdate +qall
 fi
