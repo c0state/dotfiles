95baeb8c4bc86e7aefff529307299c5c13c8f9a9
Stephen <stephen.liu@alumni.stanford.edu>
2023-03-26 23:47:58 -0400

    update dotnet

--- etc/dotnet.sh
+++ etc/dotnet.sh
@@ -12,7 +12,7 @@ if command -v dotnet >/dev/null; then
     exit 0
 fi
 
-bash -i -c "install_package https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb"
+bash -i -c "install_package https://packages.microsoft.com/config/ubuntu/22.10/packages-microsoft-prod.deb"
 
 sudo apt-get update
-sudo apt-get -y install dotnet-sdk-5.0
+sudo apt-get -y install dotnet-sdk-7.0
