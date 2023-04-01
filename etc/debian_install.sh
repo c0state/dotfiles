e529422fa180c4741202c710dc2a320d4d51110b
Stephen Liu <stephen.liu@alumni.stanford.edu>
2023-03-31 22:50:40 -0400

    add yes flag to docker install

--- etc/debian_install.sh
+++ etc/debian_install.sh
@@ -27,7 +27,7 @@ if test -z "$IS_WSL"; then
       "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 # tabby
