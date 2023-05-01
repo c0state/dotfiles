d958b24d87c9e8f776ca722380106b854457ee18
Stephen <stephen.liu@alumni.stanford.edu>
2023-04-30 21:14:27 -0400

    init only pyenv paths, don't init pyenv-virtualenv

--- .shell_interactive.sh
+++ .shell_interactive.sh
@@ -101,8 +101,6 @@ export PYENV_ROOT=$HOME/.pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
 fi
 
 export PATH=$PATH:$(python3 -m site --user-base)/bin
