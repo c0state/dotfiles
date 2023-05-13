3a5ed6918e40990d0a6129670113bf46849e8386
Stephen Liu <stephen.liu@alumni.stanford.edu>
2023-05-13 11:45:59 -0400

    add gnome settings configuration to setup scripts

--- etc/linux_tools.sh
+++ etc/linux_tools.sh
@@ -21,3 +21,9 @@ unzip "$TEMP_AWS_INSTALL_DIR"/awscli.zip -d "$TEMP_AWS_INSTALL_DIR"
 "$TEMP_AWS_INSTALL_DIR"/aws/install --install-dir "$HOME"/.local/awscli --bin-dir "$HOME"/.local/bin --update
 rm -rf "$TEMP_AWS_INSTALL_DIR"
 
+# ---------- gnome tweaks
+
+if ! which gsettings > /dev/null; then
+    # clear conflicting keybindings
+    gsettings set org.freedesktop.ibus.panel.emoji hotkey []
+fi
