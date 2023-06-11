64a1840f705e4ebd7b96148cdc8a440be2910629
Stephen <stephen.liu@alumni.stanford.edu>
2023-06-11 15:57:57 -0400

    stage rust coreutils install

--- etc/rust.sh
+++ etc/rust.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-if ! $(command -v rustc >/dev/null 2>&1); then
+set -eu
+
+if ! command -v rustc >/dev/null 2>&1; then
     curl --proto '=https' --tls-max default -sSf https://sh.rustup.rs | bash -s -- -y
 else
     rustup update
     rustup self update
 fi
 
-source "$HOME/.cargo/env"
+source "$HOME"/.cargo/env
 
+# TODO: currently broken as of 2023-06-11, not sure if ready yet...
+# cargo install coreutils ; "$HOME"/.cargo/bin/coreutils
 cargo install git-delta
 cargo install sd
 cargo install tree-sitter-cli
