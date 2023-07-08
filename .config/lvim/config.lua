d5c0c5e9ef4d25fd96e5f7c753c1d989e3a21c9b
Stephen <stephen.liu@alumni.stanford.edu>
2023-07-08 15:00:17 -0400

    use semicolon to enter command mode

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -19,6 +19,7 @@ lvim.colorscheme = "lunar"
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode[";"] = ":"
 lvim.keys.normal_mode["<leader>nt"] = ":NvimTreeToggle<CR>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
