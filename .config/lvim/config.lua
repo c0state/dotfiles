f83e43ee7decb0b8e15eb42031f1b920a7d6dd5f
Stephen <stephen.liu@alumni.stanford.edu>
2023-07-16 23:06:16 -0400

    remove tree key binding and add cd to file dir

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -22,7 +22,8 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode[";"] = ":"
-lvim.keys.normal_mode["<leader>nt"] = ":NvimTreeToggle<CR>"
+lvim.keys.normal_mode["<leader>cdf"] = ":cd %:h<CR>"
+
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
