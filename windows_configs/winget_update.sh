8350092b4ef488e494a712d779b606e718811e3c
Stephen Liu <stephen.liu@alumni.stanford.edu>
2023-08-07 19:48:27 -0400

    add signal to winget

--- windows_configs/winget_update.sh
+++ windows_configs/winget_update.sh
@@ -33,6 +33,7 @@ winget install --id mozilla.firefox
 winget install --id neovim.neovim
 winget install --id nvidia.geforceexperience
 winget install --id obsproject.obsstudio
+winget install --id openwhispersystems.signal
 winget install --id raspberrypifoundation.raspberrypiimager
 winget install --id randyrants.sharpkeys
 winget install --name rufus --source msstore
