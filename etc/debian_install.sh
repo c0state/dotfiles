1ea467ac98665a168afd4615d1fb91859507abce
Stephen <stephen.liu@alumni.stanford.edu>
2023-08-28 21:09:09 -0400

    update lvim

--- etc/debian_install.sh
+++ etc/debian_install.sh
@@ -137,7 +137,7 @@ if ! which nvim > /dev/null; then
 fi
 
 if ! which lvim > /dev/null; then
-    LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh) -- --no-install-dependencies
+    LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) -- --no-install-dependencies
 fi
 
 # install vscode
