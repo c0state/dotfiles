d6b7fee0670883b2869b112ef4ada8806a1f4eb9
Stephen <stephen.liu@alumni.stanford.edu>
2024-05-14 01:19:57 -0400

    mas signin no longer possible, add wireguard

--- etc/app_store.sh
+++ etc/app_store.sh
@@ -1,14 +1,14 @@
-mas signin --dialog
-
 mas install 497799835 # xcode
 
+# TODO: is this still relevant, commenting out 2024/05/11
 # set xcode tools to full version; see: https://stackoverflow.com/a/17980786
-echo "Setting full version of XCode tools"
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+#echo "Setting full version of XCode tools"
+#sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 
 echo "Accepting XCode license"
 sudo xcodebuild -license accept
 
 mas install 1356178125 # jigsaw outline client
 mas install 1475387142 # tailscale
+mas install 1451685025 # wireguard
 
