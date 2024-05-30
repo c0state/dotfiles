f1371db011e97f7175f48dee9592f20aeef86f77
Stephen Liu <sliu@quantum-si.com>
2024-05-30 13:26:47 -0400

    remove osxkeychain git cred helper in prepartion for using git-credential-manager

--- etc/setup_env_macos.sh
+++ etc/setup_env_macos.sh
@@ -10,3 +10,10 @@ curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.ta
 
 # for vscodevim key repeat
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# remove default usage of osxkeychain in xcode config
+# TODO: requires full disk permission for whatever term is calling or tool is editing this file
+#TMP_GITCONFIG_FILE=$(mktemp)
+#sudo gsed --null-data --regexp-extended 's/^\[credential\]\n. *?helper = osxkeychain *?\n//g' /Applications/Xcode.app/Contents/Developer/usr/share/git-core/gitconfig > $TMP_GITCONFIG_FILE
+#sudo mv $TMP_GITCONFIG_FILE /Applications/Xcode.app/Contents/Developer/usr/share/git-core/gitconfig
+
