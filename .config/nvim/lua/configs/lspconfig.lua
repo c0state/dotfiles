e539fa50b11b3e0e751586c7899432f880556040
Stephen <stephen.liu@alumni.stanford.edu>
2024-09-05 13:13:48 -0400

    update ts_ls typescript lang server

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", "ts_ls", "clangd" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -16,7 +16,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
