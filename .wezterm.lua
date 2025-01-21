d02ca589df5c54d90243f0e58adde17aa5bcd236
Stephen <stephen.liu@alumni.stanford.edu>
2025-01-21 04:11:21 -0500

    add visual bell and disable audio bell for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,4 +5,15 @@ local config = wezterm.config_builder()
 config.enable_wayland = false
 config.color_scheme = 'Darcula'
 
+config.audible_bell = "Disabled"
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+config.colors = {
+  visual_bell = '#202020',
+}
+
 return config
