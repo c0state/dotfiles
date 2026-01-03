5ea9a754887275cc75ff0d9866c4289df5df3d64
Stephen <stephen.liu@alumni.stanford.edu>
2026-01-03 16:35:23 -0500

    install sourcegit package

--- etc/debian_install.sh
+++ etc/debian_install.sh
@@ -198,6 +198,7 @@ sudo apt -y install \
     fonts-firacode \
     gh \
     lens \
+    sourcegit \
     tmux
 
 LAZYGIT_VERSION=$(get_github_release_version "https://github.com/jesseduffield/lazygit/releases/latest")
