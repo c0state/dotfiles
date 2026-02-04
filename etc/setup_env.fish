cdf70ab5dd2888a817d351787c335de89c3633a0
Stephen <stephen.liu@alumni.stanford.edu>
2026-02-04 00:07:30 -0500

    add zellij completions for fish

--- etc/setup_env.fish
+++ etc/setup_env.fish
@@ -16,6 +16,7 @@ docker completion fish > "$HOME"/.config/fish/completions/docker.fish
 k9s completion fish > "$HOME"/.config/fish/completions/k9s.fish
 kubectl completion fish > "$HOME"/.config/fish/completions/kubectl.fish
 uv generate-shell-completion fish > "$HOME"/.config/fish/completions/uv.fish
+zellij setup --generate-completion fish > "$HOME"/.config/fish/completions/zellij.fish
 
 if type -q nerdctl ; and nerdctl 2>/dev/null
   nerdctl completion fish > "$HOME"/.config/fish/completions/nerdctl.fish
