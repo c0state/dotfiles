4c471ec6425eb08660a34e43ec0edcc4375a89b9
Stephen <stephen.liu@alumni.stanford.edu>
2026-03-08 16:00:28 -0400

    add keymap to enter command mode in nvim with ";"

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+vim.keymap.set("n", ";", ":", { desc = "Enter command mode" })
 
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
