ab085536d0804888df0603791b1b36a99c56ff59
Stephen <stephen.liu@alumni.stanford.edu>
2026-04-20 15:08:31 -0400

    add libudev

--- etc/debian_install.sh
+++ etc/debian_install.sh
@@ -197,8 +197,9 @@ sudo apt -y install \
 	build-essential cmake \
 	zlib1g-dev \
 	libbz2-dev liblzma-dev \
-	libreadline-dev libsqlite3-dev libssl-dev llvm \
 	libncurses5-dev libncursesw5-dev \
+	libreadline-dev libsqlite3-dev libssl-dev llvm \
+	libudev-dev \
 	libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev \
 	libxml2-dev \
 	libyaml-dev \
