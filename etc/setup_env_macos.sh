80a69cd84e49307b709f420b87d3bd9a637aba01
Stephen <stephen.liu@alumni.stanford.edu>
2026-04-21 13:49:33 -0400

    increase spacing on macos status bar

--- etc/setup_env_macos.sh
+++ etc/setup_env_macos.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz \
-    | tar zxvf - -C "$HOME"/Library/Fonts  
-curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.tar.xz \
-    | tar zxvf - -C "$HOME"/Library/Fonts  
+# compact status bar icons to fit more and avoid notch hiding issue
+# values are half of typical baseline (12, 8); delete keys entirely to restore macOS default feel
+defaults -currentHost write -globalDomain NSStatusItemSpacing -int 6
+defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 4
+
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz |
+	tar zxvf - -C "$HOME"/Library/Fonts
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.tar.xz |
+	tar zxvf - -C "$HOME"/Library/Fonts
 
 # for vscodevim key repeat
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
@@ -13,4 +18,3 @@ defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 #TMP_GITCONFIG_FILE=$(mktemp)
 #sudo gsed --null-data --regexp-extended 's/^\[credential\]\n. *?helper = osxkeychain *?\n//g' /Applications/Xcode.app/Contents/Developer/usr/share/git-core/gitconfig > $TMP_GITCONFIG_FILE
 #sudo mv $TMP_GITCONFIG_FILE /Applications/Xcode.app/Contents/Developer/usr/share/git-core/gitconfig
-
