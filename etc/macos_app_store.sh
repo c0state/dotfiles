cbaf9283814e0c75f98e19da519ca3ff58ff3c66
Stephen <stephen.liu@alumni.stanford.edu>
2026-04-29 11:28:12 -0400

    remove obsolete xcode select call

--- etc/macos_app_store.sh
+++ etc/macos_app_store.sh
@@ -1,10 +1,5 @@
 mas install 497799835 # xcode
 
-# TODO: is this still relevant, commenting out 2024/05/11
-# set xcode tools to full version; see: https://stackoverflow.com/a/17980786
-#echo "Setting full version of XCode tools"
-#sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-
 echo "Accepting XCode license"
 sudo xcodebuild -license accept
 
